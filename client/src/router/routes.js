1d902cebc924c0b08d29d61ee0fd7780397bb806
e <2946066962@qq.com>
2020-07-13 10:05:31 +0800

    粉笔项目环境

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -2,5 +2,19 @@
 	// login:"http://localhost:8080"
 // }
 export default{
-	login:"http://localhost:8080"
+	//TopNav的网址
+	//如 login:192.168.3.112:8080
+	//使用：this.$routes.login
+	
+	//FootNav的网址
+	
+	//home的网址
+	
+	//Course的网址
+	
+	//Download的网址
+	
+	//Questions的网址
+	
+	//Teachers的网址
 }
